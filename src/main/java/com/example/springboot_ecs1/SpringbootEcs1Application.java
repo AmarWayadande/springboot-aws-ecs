2958c7d4de67c34c975526db5f6bd2c999cbabcd
Shreetej <amarwayadande4231@gmail.com>
2024-09-09 16:10:05 +0530

    first ecs1 commit

---
+++ src/main/java/com/example/springboot_ecs1/SpringbootEcs1Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ecs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEcs1Application.class, args);
+	}
+
+}
